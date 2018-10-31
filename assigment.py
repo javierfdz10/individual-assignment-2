b8929d633995414d6192b14e7486a7bfff0eaa80
Javier <javier.fdz@student.ie.edu>
2018-10-31 13:55:03 +0100

    add bubblesort function

--- assigment.py
+++ assigment.py
@@ -6,3 +6,17 @@ Created on Fri Oct 26 15:19:12 2018
 @author: Javier
 """
 
+#%%
+
+# Bubble sort implementation.
+#
+# This function returns nothing because it works by mutating the lst.
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
+#%%
